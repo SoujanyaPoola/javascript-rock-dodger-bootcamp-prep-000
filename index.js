1b7ce729510e7b9f31a639e482419fbd61476994
Souji 2613 <puppypoola@gmail.com>
2018-06-18 15:49:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -91,6 +91,11 @@ if (checkCollision(rock)) {
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM
      */
+      if (top < GAME_HEIGHT) {
+       window.requestAnimationFrame(moveRock)
+     } else {
+       rock.remove()
+     }
   }
 
   // We should kick of the animation of the rock around here
